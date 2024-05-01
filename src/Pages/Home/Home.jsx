d32930a5f34771a198bea0bebb1a82e4a3c60575
OmkarPatil50 <Omkarp5429@gmail.com>
2024-05-01 18:17:23 +0530

    feat(Component): :sparkles: Add spinner and fix error

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Sidebar from "../../Components/Sidebar/Sidebar";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllJobs } from "../../Thunks/jobsThunk";
 import JobCard from "../../Components/JobCard/JobCard";
+import Spinner from "../../Components/Spinner/Spinner";
 import "./Home.css";
 import JobModal from "../../Components/Modal/Modal";
 
@@ -20,13 +21,20 @@ const Home = () => {
     dispatch(getAllJobs(page));
   }, []);
 
+  let fetchData = true;
+
   const handleInfiniteScroll = async () => {
     try {
       if (
         window.innerHeight + document.documentElement.scrollTop + 1 >=
         document.documentElement.scrollHeight
       ) {
-        dispatch(getAllJobs(page));
+        if (fetchData) {
+          fetchData = false;
+          dispatch(getAllJobs(page)).then(() => {
+            fetchData = true;
+          });
+        }
       }
     } catch (error) {
       console.log(error);
@@ -36,7 +44,7 @@ const Home = () => {
   useEffect(() => {
     window.addEventListener("scroll", handleInfiniteScroll);
     return () => window.removeEventListener("scroll", handleInfiniteScroll);
-  }, [page]);
+  }, []);
 
   const handleOpenModal = (data) => {
     setOpenModal((prev) => ({ ...prev, open: true, data }));
@@ -67,7 +75,13 @@ const Home = () => {
             );
           })}
         </div>
-        {loading ? <h2>Loading...!</h2> : ""}
+        {loading ? (
+          <div className="spinner-home flex-center">
+            <Spinner />
+          </div>
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
